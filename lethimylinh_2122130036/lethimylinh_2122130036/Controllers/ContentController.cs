6bf7cbc6f82b64100f06b271b544eee63f41072c
linhltml <linhltm8601@gmail.com>
2024-12-21 13:54:01 +0700

    update

---
+++ lethimylinh_2122130036/lethimylinh_2122130036/Controllers/ContentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lethimylinh_2122130036.Controllers
+{
+    public class ContentController : Controller
+    {
+        // GET: Content
+        public ActionResult Concent()
+        {
+            return View();
+        }
+    }
+}
